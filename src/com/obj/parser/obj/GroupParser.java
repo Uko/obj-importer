14e231db17e232a4a091f966f40585c6675b49e1
Uko <uko@unikernel.net>
2012-10-01 18:18:26 +0300

    Fixed parsing of the groups without a name

--- src/com/obj/parser/obj/GroupParser.java
+++ src/com/obj/parser/obj/GroupParser.java
@@ -6,25 +6,30 @@ import com.obj.parser.LineParser;
 
 public class GroupParser extends LineParser {
 
-	Group newGroup = null;
-	
-	@Override
-	public void incoporateResults(WavefrontObject wavefrontObject) {
-		
-		if (wavefrontObject.getCurrentGroup() != null)
-			wavefrontObject.getCurrentGroup().pack();
-		
-		wavefrontObject.getGroups().add(newGroup);
-		wavefrontObject.getGroupsDirectAccess().put(newGroup.getName(),newGroup);
-		
-		wavefrontObject.setCurrentGroup(newGroup);
-	}
+    Group newGroup = null;
 
-	@Override
-	public void parse() {
-		
-		String groupName = words[1];
-		newGroup = new Group(groupName);
-	}
+    @Override
+    public void incoporateResults(WavefrontObject wavefrontObject) {
+
+        if (wavefrontObject.getCurrentGroup() != null) {
+            wavefrontObject.getCurrentGroup().pack();
+        }
+
+        wavefrontObject.getGroups().add(newGroup);
+        wavefrontObject.getGroupsDirectAccess().put(newGroup.getName(), newGroup);
+
+        wavefrontObject.setCurrentGroup(newGroup);
+    }
+
+    @Override
+    public void parse() {
+        String groupName;
+        if (words.length == 1) {
+            groupName = "default";
+        } else {
+            groupName = words[1];
+        }
+        newGroup = new Group(groupName);
+    }
 
 }
